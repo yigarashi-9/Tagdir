d7b2656514dd987959c7941bd7561a1f5deb05d0
yu-i9 <yuu.igarashi.9@gmail.com>
2018-12-11 10:40:10 +0900

    Write test for getxattr

---
+++ tests/test_tagdir_getxattr.py
@@ -0,0 +1,46 @@
+from errno import ENODATA
+
+import pytest
+
+from .conftest import setup_tagdir_test
+from tagdir.tagdir import ENTINFO_PATH
+from tagdir.fusepy.fuse import ENOTSUP
+from tagdir.fusepy.exceptions import FuseOSError
+from tagdir.models import Attr, Entity, Tag
+
+
+def setup_func(session):
+    attr1 = Attr.new_tag_attr()
+    attr2 = Attr.new_tag_attr()
+    tag1 = Tag("tag1", attr1)
+    tag2 = Tag("tag2", attr2)
+    attr3 = Attr.new_entity_attr()
+    entity1 = Entity("entity1", attr3, "/path1", [tag1, tag2])
+    session.add_all([attr1, attr2, attr3, tag1, tag2, entity1])
+
+
+# Dynamically define tagdir fixtures
+setup_tagdir_test(setup_func, "getxattr")
+
+
+def test_normal(tagdir):
+    expected = b"/path1,tag1,tag2"
+    assert tagdir.getxattr(ENTINFO_PATH, "entity1") == expected
+
+
+def test_nonexistent_entity(tagdir):
+    with pytest.raises(FuseOSError) as exc:
+        tagdir.getxattr(ENTINFO_PATH, "nonexistent")
+    assert exc.value.errno == ENODATA
+
+
+@pytest.mark.parametrize("input", [
+    "/",
+    "/@tag1/@tag2",
+    "/@tag1/entity1",
+    "/@non_tag",
+])
+def test_invalid_path(tagdir, input):
+    with pytest.raises(FuseOSError) as exc:
+        tagdir.getxattr(input, "fail")
+    assert exc.value.errno == ENOTSUP
